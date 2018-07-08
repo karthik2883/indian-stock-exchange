6bf2ef703a1b9c7d2b1bd06983497a7ee38cf12e
Nishchay Kaushik <kaushiknishchay@gmail.com>
2018-07-08 16:29:22 +0530

    Chnage http to https in urls

--- nse/constant.js
+++ nse/constant.js
@@ -1,10 +1,10 @@
-exports.INDEX_URL = 'http://www.nseindia.com/homepage/Indices1.json';
+exports.INDEX_URL = 'https://www.nseindia.com/homepage/Indices1.json';
 exports.SECTORS_LIST = 'https://nseindia.com/homepage/peDetails.json';
-exports.STOCKS_CSV_URL = 'http://www.nseindia.com/content/equities/EQUITY_L.csv';
+exports.STOCKS_CSV_URL = 'https://www.nseindia.com/content/equities/EQUITY_L.csv';
 exports.MARKET_STATUS_URL ='https://nseindia.com//emerge/homepage/smeNormalMktStatus.json';
-exports.ADVANCES_DECLINES_URL = 'http://www.nseindia.com/common/json/indicesAdvanceDeclines.json';
-exports.LOSERS_URL = 'http://www.nseindia.com/live_market/dynaContent/live_analysis/losers/niftyLosers1.json';
-exports.GAINERS_URL = 'http://www.nseindia.com/live_market/dynaContent/live_analysis/gainers/niftyGainers1.json';
+exports.ADVANCES_DECLINES_URL = 'https://www.nseindia.com/common/json/indicesAdvanceDeclines.json';
+exports.LOSERS_URL = 'https://www.nseindia.com/live_market/dynaContent/live_analysis/losers/niftyLosers1.json';
+exports.GAINERS_URL = 'https://www.nseindia.com/live_market/dynaContent/live_analysis/gainers/niftyGainers1.json';
 exports.GET_QUOTE_URL = 'https://nseindia.com/live_market/dynaContent/live_watch/get_quote/GetQuote.jsp?symbol=';
 exports.INDICES_WATCH_URL = 'https://nseindia.com/live_market/dynaContent/live_watch/stock_watch/liveIndexWatchData.json';
 exports.QUOTE_INFO_URL = 'https://nseindia.com/live_market/dynaContent/live_watch/get_quote/ajaxGetQuoteJSON.jsp?series=EQ&symbol=';
